06214298fe717690e57921c646a0490204ad627e
Luio <lcastrogiovanni@itba.edu.ar>
2017-10-10 23:27:03 -0300

    Add files via upload

---
+++ TP6-Networking/TP6-Networking/userData.h
@@ -0,0 +1,12 @@
+#ifndef USERDATA_H
+#define USERDATA_H
+#include <string>
+
+	typedef struct userDataType{
+		userDataType *Flag;
+		string myIP;
+		bool inicia;
+		//string sequence;
+	} userData;
+
+#endif
